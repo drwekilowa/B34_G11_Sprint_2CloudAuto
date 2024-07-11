8609105fd1bafe1b071e3d4761e67b7c9b82b10f
Gabriel Hernandez <gaboherdez09@gmail.com>
2024-07-11 19:35:34 -0400

    Steps definitions

--- src/test/java/com/tryCloud/step_definitions/US010_FolderViewFunc_StepDef.java
+++ src/test/java/com/tryCloud/step_definitions/US010_FolderViewFunc_StepDef.java
@@ -1,12 +1,15 @@
 package com.tryCloud.step_definitions;
 
+import com.tryCloud.pages.BasePage;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class US010_FolderViewFunc_StepDef {
+    BasePage basePage = new BasePage();
+
     @When("User clicks on the folder icon")
     public void user_clicks_on_the_folder_icon() {
-
+    basePage.fileModule.click();
     }
     @When("User clicks Name above the files.")
     public void user_clicks_name_above_the_files() {
