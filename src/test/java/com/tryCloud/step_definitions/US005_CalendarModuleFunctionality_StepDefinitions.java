462d6f3f2d5e869a4119f535664ff0f3493347c1
Ssevinj <neroli.fashion@gmail.com>
2024-07-11 22:14:25 -0400

    Updates added to calendar module step definition

--- src/test/java/com/tryCloud/step_definitions/US005_CalendarModuleFunctionality_StepDefinitions.java
+++ src/test/java/com/tryCloud/step_definitions/US005_CalendarModuleFunctionality_StepDefinitions.java
@@ -5,6 +5,9 @@ import io.cucumber.java.en.When;
 
 public class US005_CalendarModuleFunctionality_StepDefinitions {
 
+// @B34G11-181 @all
+//  Scenario: US005_TC1_Verify user can display daily calendar view
+
 
 
 
