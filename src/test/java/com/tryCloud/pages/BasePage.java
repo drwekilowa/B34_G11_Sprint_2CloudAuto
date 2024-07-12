e721c2096039ea8da21be0505e45a36410aa2905
drwekilowa <drwekilowa@gmail.com>
2024-07-11 21:58:18 -0400

    add Dynamic Locator method

--- src/test/java/com/tryCloud/pages/BasePage.java
+++ src/test/java/com/tryCloud/pages/BasePage.java
@@ -1,13 +1,15 @@
 package com.tryCloud.pages;
 
 import com.tryCloud.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
-    public void BasePage() {
+    public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
@@ -51,5 +53,14 @@ public class BasePage {
 
     @FindBy(xpath = "//div[@class='menutoggle']")
     public WebElement userIcon;
+
+// this method only useful module links
+    public void dynamicLinkElement(String text){
+
+        String locator ="//a[@aria-label=('"+text+"')]";
+        WebElement element = Driver.getDriver().findElement(By.xpath(locator));
+
+        Assert.assertTrue("DynamicLinkElement method Failed!",element.isDisplayed());
+    }
 
 }
