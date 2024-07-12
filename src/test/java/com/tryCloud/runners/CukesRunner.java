d622b0518ff91b59fa6cf1ed4c08dcd1f845a4fb
Ssevinj <161057036+Ssevinj@users.noreply.github.com>
2024-07-12 13:38:12 -0400

    Update CukesRunner.java

--- src/test/java/com/tryCloud/runners/CukesRunner.java
+++ src/test/java/com/tryCloud/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
-        features = "src/test/resources/Features/US005_CalendarModuleFunctionality.feature",
+        features = "src/test/resources/Features",
         glue = "com/tryCloud/step_definitions",
         dryRun = false,
         tags = "",
