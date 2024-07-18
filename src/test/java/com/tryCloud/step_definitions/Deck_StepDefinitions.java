d6561ede8e4844bbd261e768580eea0a67933e27
dylansg <dylansg@yahoo.com>
2024-07-18 04:05:58 -0400

    Added Deck Module

---
+++ src/test/java/com/tryCloud/step_definitions/Deck_StepDefinitions.java
@@ -0,0 +1,31 @@
+package com.tryCloud.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class Deck_StepDefinitions {
+    @When("User clicks on deck button")
+    public void user_click_deck_button(){
+
+    }
+
+    @And("User clicks add board button")
+    public void userClicksAddBoardButton() {
+
+    }
+
+    @And("User types a title")
+    public void userTypesATitle() {
+
+    }
+
+    @And("User can see new board")
+    public void userCanSeeNewBoard() {
+
+    }
+
+    @Then("User can modify the board")
+    public void userCanModifyTheBoard() {
+    }
+}
---
+++ src/test/resources/Features/DeckModuleFunctionality.feature
@@ -0,0 +1,18 @@
+@DeckModule
+Feature: Deck Module Functionality
+
+  User Story: As a user, I should be able to manage my works by creating a new board/list/card and modifying them under Deck Module.
+
+  Background:
+    Given user is on the login page
+    When user logged in as "user"
+
+  @AddBoardDeck
+  Scenario: User can create a new board under deck module and modify
+    When User clicks on deck button
+    And User clicks add board button
+    And User types a title
+    And User can see new board
+    Then User can modify the board
+
+
