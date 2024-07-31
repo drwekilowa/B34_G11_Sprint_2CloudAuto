03320951a5603b2adb5139831f75c31ffa6d3b41
Sole1993 <Cilkasokic@gmail.com>
2024-07-31 11:30:21 -0400

    Adding Test Cases US012 Add File to favorite,Rename,Comment.

--- src/test/java/com/tryCloud/step_definitions/US012_AddFileToFavorite.java
+++ src/test/java/com/tryCloud/step_definitions/US012_AddFileToFavorite.java
@@ -1,50 +1,71 @@
 package com.tryCloud.step_definitions;
 
-import com.tryCloud.pages.BasePage;
 import com.tryCloud.pages.FileModule_S_Page;
-import com.tryCloud.pages.LoginPage;
-import com.tryCloud.utilities.ConfigurationReader;
-import com.tryCloud.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-
-import java.util.List;
+import org.openqa.selenium.Keys;
 
 public class US012_AddFileToFavorite extends FileModule_S_Page {
 
 
+// AC1 User should be able to add file to favorite
     @Given("The user navigates to the files page")
     public void user_navigates_to_files_page() {
-
         fileLinkModule.click();
-
     }
 
     @And("User clicks 3 dots menu")
     public void User_clicks_3_dot_menu() {
-
-       threeDotsMenu.click();
+        threeDotsIcon.click();
 
     }
     @Then("User adds his file to favorite")
     public void user_adds_his_file_to_favorite() {
-
       addToFavorites.click();
 
     }
     @Then("User should be able to see the file under the Favorites tab")
     public void userShouldBeAbleToSeeTheFileUnderTheFavoritesTab() {
+    }
+
+
+    // AC2 Users can rename any file from its own three dots menu
+
+    @And("User clicks on rename the file")
+    public void user_clicks_on_rename_file() {
+        RenameFile.click();
+        fileNameInput.sendKeys("8");
+        fileNameInput.sendKeys(Keys.RETURN);
+    }
+    // AC3 User can put some comments on any file from the file details menu opened right side
+
+   @And ("Click on \"Details\"")
+    public void click_on_details() {
+    Details.click();
+    Comment.click();
+    Comment1.click();
+    Comment1.sendKeys("asdas");
+
+
+   }
+
+
+
+
+
 
 
 
     }
-}
+
+
+
+
+
+
+
+
+
 
 
--- src/test/resources/Features/US012_AddFileToFavouriteRenameComment.feature
+++ src/test/resources/Features/US012_AddFileToFavouriteRenameComment.feature
@@ -1,17 +1,32 @@
+
 Feature: Add file to Favorite,Rename and Comment
 
   User Story: As a user, I should be able to add a file to favorites, rename any file and give some comments on any file.
 
-
-Background:
+  Background
   Given user is on the login page
   When user logged in as "user"
 
+  @B34G11-206
   Scenario: AC1 User should be able to add file to favorite
 
-  Given The user navigates to the files page
-  And User clicks 3 dots menu
-  Then User adds his file to favorite
-  Then User should be able to see the file under the Favorites tab
+    Given The user navigates to the files page
+    And User clicks 3 dots menu
+    Then User adds his file to favorite
+    Then User should be able to see the file under the Favorites tab
+
+  @B34G11-208
+  Scenario: AC2 Users can rename any file from its own three dots menu
+    Given The user navigates to the files page
+    And User clicks 3 dots menu
+    And User clicks on rename the file
+
+  @B34G11-209
+  Scenario: AC3 User can put some comments on any file from the file details menu opened right side.
+    Given The user navigates to the files page
+    And User clicks 3 dots menu
+    And Click on "Details"
+
+
 
 
